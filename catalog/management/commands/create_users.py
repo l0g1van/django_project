136f5733dae10b9fecf9a392904025b8d839cb5c
Ivan <ivankucevalov@gmail.com>
2023-01-27 13:01:41 +0200

    home-task 8 3rd commit

--- catalog/management/commands/create_users.py
+++ catalog/management/commands/create_users.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from catalog.models import Users
+from django.contrib.auth.models import User
 from faker import Faker
 
 fake = Faker()
@@ -12,6 +13,11 @@ class Command(BaseCommand):
         parser.add_argument('arg_1', type=int, choices=range(1, 11))
 
     def handle(self, *args, **options):
-
-        for el in range(options['arg_1'][0]):
-            Users.objects.create(name=fake.name(), email=fake.email(), password=fake.password())
+        for _ in range(options['arg_1']):
+            fake_name = fake.first_name()
+            fake_last_name = fake.last_name()
+            User.objects.bulk_create([User(first_name=fake_name,
+                                           email=f'{fake_name}.{fake_last_name}@{fake.domain_name()}',
+                                           password=fake.password(),
+                                           last_name=fake_last_name,
+                                           username=f'{fake_name}.{fake_last_name}')])
--- catalog/management/commands/delete_users.py
+++ catalog/management/commands/delete_users.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 
 import catalog.models
 from catalog.models import Users
+from django.contrib.auth.models import User
 
 
 class Command(BaseCommand):
@@ -10,12 +11,9 @@ class Command(BaseCommand):
         parser.add_argument('arg_1', nargs='+', type=int)
 
     def handle(self, *args, **options):
-        try:
-            for el in options['arg_1']:
-                if Users.objects.filter(id=el).get() not in Users.objects.filter(superuser=1):
-                    Users.objects.filter(id=el, superuser=0).delete()
-                    self.stdout.write(self.style.SUCCESS(f'output: {Users.objects.filter(id=el, superuser=0).get()}'))
-                else:
-                    self.stdout.write(self.style.SUCCESS('There is one or more superusers with that ids'))
-        except catalog.models.Users.DoesNotExist:
-            pass
+        q = User.objects.filter(id__in=options['arg_1'])
+        if q.filter(is_superuser=True).exists():
+            self.stdout.write(self.style.SUCCESS('There is one or more superusers so I cant delete this items'))
+        else:
+            q.delete()
+            self.stdout.write(self.style.SUCCESS('Items was successfully deleted'))
