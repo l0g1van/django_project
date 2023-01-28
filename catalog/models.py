90c81980618246197a2924d095e0866afe8da4e8
Ivan <ivankucevalov@gmail.com>
2023-01-28 18:34:09 +0200

    home-task 9 commit

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 
 class Users(models.Model):
@@ -10,3 +11,36 @@ class Users(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class City(models.Model):
+
+    name = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    continent = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=75)
+
+
+class Customer(models.Model):
+
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    product = models.ManyToManyField(Product)
+    name = models.CharField(max_length=75)
+    last_name = models.CharField(max_length=75)
+    address = models.CharField(max_length=75)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Phone number must be entered in the format: '+999999999'. "
+                                         "Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+
+
+class Retailer(models.Model):
+    name = models.CharField(max_length=50)
+    city = models.OneToOneField(City, on_delete=models.CASCADE, default=1)
