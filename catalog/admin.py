eb14bab8b0e8593cc400129a5ce7b21d1b4a9c8e
kutse_0kfg5db <ivankucevalov@gmail.com>
2023-02-21 11:45:05 +0200

    home-task 15 admin added

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from catalog.models import Logs
+from catalog.models import Logs, AuthorAndQuote
 
 
 @admin.register(Logs)
@@ -13,3 +13,14 @@ class LogsAdmin(admin.ModelAdmin):
                  ]
     list_filter = ['date_time']
     search_fields = ['method', 'path']
+
+
+@admin.register(AuthorAndQuote)
+class AuthorAndQuoteAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quote')
+
+    fieldsets = [
+        (None, {'fields': ['name', 'quote']}),
+        ('Authors birth date', {'fields': ['birth_date']}),
+        ('Author details', {'fields': ['details']})
+    ]
